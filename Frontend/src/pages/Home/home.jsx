4ea4490d672fd942432ea3b5057a7bc2bf8de824
SupeshUgale <supeshugale31@gmail.com>
2026-06-13 15:26:40 +0530

    updaet

--- Frontend/src/pages/Home/home.jsx
+++ Frontend/src/pages/Home/home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 
 const Home = () => {
-  const [category , setCategory] =useState("All");
+  const [category, setCategory] = useState("All");
+
   return (
     <div className="home">
       <Header />
-      <ExploreMenu category={category} setCategory={setCategory} />
+      <ExploreMenu
+        category={category}
+        setCategory={setCategory}
+      />
     </div>
   );
 };
